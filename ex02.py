e334be2f8b93739f85b5260476ebfd6b107572e5
Maria Vitoria Cosmo <121966349+vitoria-cosmos@users.noreply.github.com>
2023-07-14 10:09:35 -0300

    Create ex02.py

---
+++ ex02.py
@@ -0,0 +1,6 @@
+dia = input('Qual o seu dia de nascimento? ')
+mes = input('Em que mês voce nasceu? ')
+ano = input('Em que ano voce nasceu? ')
+print('Voce nasceu no dia ',dia,'no mes ',mes,'no ano de ',ano)
+
+
